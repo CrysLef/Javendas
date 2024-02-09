a54b19f59c7fc07aa85dd3c9815392dab1146740
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:38:12 -0300

    Cria interface do repositorio de cliente

---
+++ src/main/java/br/com/cryslefundes/repository/IClienteRepository.java
@@ -0,0 +1,7 @@
+package main.java.br.com.cryslefundes.repository;
+
+import main.java.br.com.cryslefundes.domain.Cliente;
+import main.java.br.com.cryslefundes.repository.generic.IGenericRepository;
+
+public interface IClienteRepository extends IGenericRepository<Cliente, String> {
+}
