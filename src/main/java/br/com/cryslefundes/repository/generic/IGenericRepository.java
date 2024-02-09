f5deef74bab504fdfb27b5a83f5d7b8b0d3bc1d2
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:34:52 -0300

    Cria interface generica de repositorio

---
+++ src/main/java/br/com/cryslefundes/repository/generic/IGenericRepository.java
@@ -0,0 +1,16 @@
+package main.java.br.com.cryslefundes.repository.generic;
+
+import main.java.br.com.cryslefundes.exceptions.TipoChaveNaoEncontradaException;
+import main.java.br.com.cryslefundes.repository.Persistente;
+
+import java.util.Collection;
+
+public interface IGenericRepository<T extends Persistente, E> {
+    Boolean cadastrar(T entidade) throws TipoChaveNaoEncontradaException;
+    void excluir(E valor);
+    void alterar(T entidade) throws TipoChaveNaoEncontradaException;
+    T consultar(E valor);
+
+    Collection<T> buscarTodos();
+
+}
