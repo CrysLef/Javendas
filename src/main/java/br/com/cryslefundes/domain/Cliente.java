efeedec7d9049855c317dbb1782498a785ad46a3
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:29:25 -0300

    Cria dominio Cliente

---
+++ src/main/java/br/com/cryslefundes/domain/Cliente.java
@@ -0,0 +1,62 @@
+package main.java.br.com.cryslefundes.domain;
+
+import main.java.annotation.TipoChave;
+import main.java.br.com.cryslefundes.repository.Persistente;
+
+public class Cliente implements Persistente {
+    @TipoChave("getCpf")
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String cidade;
+    private String estado;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
