610cb5994dae453d875fea5ca7e9abc0e0d0c353
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:44:01 -0300

    Cria implementacao da interface de service do produto

---
+++ src/main/java/br/com/cryslefundes/service/ProdutoService.java
@@ -0,0 +1,11 @@
+package main.java.br.com.cryslefundes.service;
+
+import main.java.br.com.cryslefundes.domain.Produto;
+import main.java.br.com.cryslefundes.repository.IProdutoRepository;
+import main.java.br.com.cryslefundes.service.generic.GenericService;
+
+public class ProdutoService extends GenericService<Produto, String> implements IProdutoService {
+    public ProdutoService(IProdutoRepository ProdutoRepository) {
+        super(ProdutoRepository);
+    }
+}
