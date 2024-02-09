68035b08f5f76675f69b26ea039b0dbf632cc315
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:39:47 -0300

    Cria interface do repositorio de produto

---
+++ src/main/java/br/com/cryslefundes/repository/IProdutoRepository.java
@@ -0,0 +1,7 @@
+package main.java.br.com.cryslefundes.repository;
+
+import main.java.br.com.cryslefundes.domain.Produto;
+import main.java.br.com.cryslefundes.repository.generic.IGenericRepository;
+
+public interface IProdutoRepository extends IGenericRepository<Produto, String> {
+}
