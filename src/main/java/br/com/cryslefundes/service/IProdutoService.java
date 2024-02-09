09b1a402f1bad72b9f1cf0c15610ecb87a5bbd43
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:43:47 -0300

    Cria interface de service do produto

---
+++ src/main/java/br/com/cryslefundes/service/IProdutoService.java
@@ -0,0 +1,7 @@
+package main.java.br.com.cryslefundes.service;
+
+import main.java.br.com.cryslefundes.domain.Produto;
+import main.java.br.com.cryslefundes.service.generic.IGenericService;
+
+public interface IProdutoService extends IGenericService<Produto, String> {
+}
