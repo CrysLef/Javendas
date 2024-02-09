71dcd93da99a58c4529bdcbe22f093d9457b9187
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-09 18:40:06 -0300

    Cria repositorio de produto baseado na classe abstrata de repositorio

---
+++ src/main/java/br/com/cryslefundes/repository/ProdutoRepository.java
@@ -0,0 +1,24 @@
+package main.java.br.com.cryslefundes.repository;
+
+import main.java.br.com.cryslefundes.domain.Produto;
+import main.java.br.com.cryslefundes.repository.generic.GenericRepository;
+
+public class ProdutoRepository extends GenericRepository<Produto, String> implements IProdutoRepository {
+
+    public ProdutoRepository() {
+        super();
+    }
+
+    @Override
+    public Class<Produto> getTipoClasse() {
+        return Produto.class;
+    }
+
+    @Override
+    public void atualizarDados(Produto entidade, Produto entidadeCadastrado) {
+        entidadeCadastrado.setNome(entidade.getNome());
+        entidadeCadastrado.setCodigo(entidade.getCodigo());
+        entidadeCadastrado.setDescricao(entidade.getDescricao());
+        entidadeCadastrado.setValor(entidade.getValor());
+    }
+}
