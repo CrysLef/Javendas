faba38dfda7252ff91e1e4db382f57e21274be81
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-19 20:10:05 -0300

    Criando testes mockados de service

--- src/test/java/br/com/cryslefundes/service/ClienteServiceTest.java
+++ src/test/java/br/com/cryslefundes/service/ClienteServiceTest.java
@@ -1,19 +1,24 @@
 package test.java.br.com.cryslefundes.service;
 
 import main.java.br.com.cryslefundes.domain.Cliente;
-import main.java.br.com.cryslefundes.exceptions.TipoChaveNaoEncontradaException;
 import main.java.br.com.cryslefundes.repository.IClienteRepository;
 import main.java.br.com.cryslefundes.service.ClienteService;
 import main.java.br.com.cryslefundes.service.IClienteService;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import test.java.br.com.cryslefundes.repository.mock.ClienteRepositoryMock;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 
 public class ClienteServiceTest {
     private IClienteService clienteService;
-    private Cliente cliente;
+    private Cliente cliente1;
+    private Cliente cliente2;
+    private Collection<Cliente> clientes = new ArrayList<>();
 
     public ClienteServiceTest() {
         IClienteRepository repository = new ClienteRepositoryMock();
@@ -22,37 +27,74 @@ public class ClienteServiceTest {
 
     @Before
     public void init() {
-        cliente = new Cliente();
-        cliente.setCpf(44124124124L);
-        cliente.setNome("Crystian");
-        cliente.setCidade("Rio de janeiro");
-        cliente.setEndereco("Rua dos bobos");
-        cliente.setEstado("RJ");
-        cliente.setTelefone("219312387423");
+        // cliente 1:
+        cliente1 = new Cliente();
+        cliente1.setCpf(44124124124L);
+        cliente1.setNome("Crystian");
+        cliente1.setCidade("Rio de janeiro");
+        cliente1.setEndereco("Rua dos bobos, 0");
+        cliente1.setEstado("RJ");
+        cliente1.setTelefone("+55 (21) 92312-3423");
+        cliente1.setEmail("funcionario1@admin.com");
+
+        // cliente 2:
+        cliente2 = new Cliente();
+        cliente2.setCpf(31178964156L);
+        cliente2.setNome("Juan");
+        cliente2.setCidade("Belo Horizonte");
+        cliente2.setEndereco("Avenida Inexistente, 87");
+        cliente2.setEstado("MG");
+        cliente2.setTelefone("+55 (31) 98491-2042");
+        cliente2.setEmail("funcionario2@admin.com");
+    }
+
+    @After
+    public void truncateClientes() {
+        clientes = clienteService.buscarTodos();
+        if (clientes != null) {
+            clientes.forEach(c -> clienteService.excluir(c.getCpf()));
+        }
+    }
+    
+    @Test
+    public void validaSalvarCliente() {
+        clientes.add(cliente1);
+        clientes.add(cliente2);
+        clientes.forEach(c -> {
+            Boolean isCadastrado = clienteService.cadastrar(c);
+            Assert.assertTrue(isCadastrado);
+        });
     }
 
     @Test
-    public void validaPesquisarCliente() {
-        Cliente clienteConsultado = clienteService.consultar(cliente.getCpf());
-        Assert.assertNotNull(clienteConsultado);
+    public void validaConsultarCliente() {
+        clientes = clienteService.buscarTodos();
+        clientes.forEach(c -> {
+            Cliente clienteConsultado = clienteService.consultar(c.getCpf());
+            Assert.assertNotNull(clienteConsultado);
+        });
     }
 
     @Test
-    public void validaSalvarCliente() throws TipoChaveNaoEncontradaException {
-        Boolean retornoCadastro = clienteService.cadastrar(cliente);
-        Assert.assertTrue(retornoCadastro);
+    public void validaAlterarCliente() {
+        cliente1.setNome("Crystian Lefundes");
+        clienteService.alterar(cliente1);
+        Assert.assertEquals("Crystian Lefundes", cliente1.getNome());
+    }
+
+    @Test
+    public void validaBuscarTodosClientes() {
+        clientes = clienteService.buscarTodos();
+        Assert.assertNotNull(clientes);
     }
 
     @Test
     public void validaExcluirCliente() {
-        clienteService.excluir(cliente.getCpf());
-    }
-
-    @Test
-    public void validaAlterarCliente() throws TipoChaveNaoEncontradaException {
-        cliente.setNome("Crystian Lefundes");
-        clienteService.alterar(cliente);
-        Assert.assertEquals("Crystian Lefundes", cliente.getNome());
+        clientes = clienteService.buscarTodos();
+        clientes.forEach(c -> {
+            clienteService.excluir(c.getCpf());
+            Cliente clienteConsultado = clienteService.consultar(c.getCpf());
+        });
     }
 
 }
--- src/test/java/br/com/cryslefundes/service/ProdutoServiceTest.java
+++ src/test/java/br/com/cryslefundes/service/ProdutoServiceTest.java
@@ -1,20 +1,24 @@
 package test.java.br.com.cryslefundes.service;
 
 import main.java.br.com.cryslefundes.domain.Produto;
-import main.java.br.com.cryslefundes.exceptions.TipoChaveNaoEncontradaException;
 import main.java.br.com.cryslefundes.repository.IProdutoRepository;
 import main.java.br.com.cryslefundes.service.IProdutoService;
 import main.java.br.com.cryslefundes.service.ProdutoService;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import test.java.br.com.cryslefundes.repository.mock.ProdutoRepositoryMock;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class ProdutoServiceTest {
     private IProdutoService produtoService;
-    private Produto produto;
+    private Produto produto1;
+    private Produto produto2;
+    private Collection<Produto> produtos = new ArrayList<>();
 
     public ProdutoServiceTest() {
         IProdutoRepository repository = new ProdutoRepositoryMock();
@@ -23,38 +27,71 @@ public class ProdutoServiceTest {
 
     @Before
     public void init() {
-        produto = new Produto();
-        produto.setCodigo(418263L);
-        produto.setNome("Produto 1");
-        produto.setDescricao("Produto 1");
-        produto.setValor(BigDecimal.TEN);
+        // produto 1:
+        produto1 = new Produto();
+        produto1.setCodigo(418263L);
+        produto1.setNome("Smartphone ruim");
+        produto1.setDescricao("Um celular horrível por preço de banana");
+        produto1.setValor(BigDecimal.TEN);
+        produto1.setEstoque(42);
+
+        // produto 2:
+        produto2 = new Produto();
+        produto2.setCodigo(529374L);
+        produto2.setNome("Notebook caro");
+        produto2.setDescricao("Um notebook muito caro que não vale o preço");
+        produto2.setValor(BigDecimal.TEN);
+        produto2.setEstoque(23);
+    }
+
+    @After
+    public void truncateProdutos() {
+        produtos = produtoService.buscarTodos();
+        if (produtos != null) {
+            produtos.forEach(p -> produtoService.excluir(p.getCodigo()));
+        }
     }
 
     @Test
-    public void validaPesquisarProduto() {
-        Produto produto = this.produtoService.consultar(this.produto.getCodigo());
-
-        Assert.assertNotNull(produto);
+    public void validaSalvarProduto() {
+        produtos.add(produto1);
+        produtos.add(produto2);
+        produtos.forEach(p -> {
+            Boolean isCadastrado = produtoService.cadastrar(p);
+            Assert.assertTrue(isCadastrado);
+        });
     }
 
     @Test
-    public void validaSalvarProduto() throws TipoChaveNaoEncontradaException {
-        Boolean valorRetorno = produtoService.cadastrar(produto);
+    public void validaConsultarProduto() {
+        produtos = produtoService.buscarTodos();
+        produtos.forEach(p -> {
+            Produto produtoConsultado = produtoService.consultar(p.getCodigo());
+            Assert.assertNotNull(produtoConsultado);
+        });
+    }
 
-        Assert.assertTrue(valorRetorno);
+    @Test
+    public void validaAlterarProduto() {
+        produto1.setNome("Celular bom");
+        produtoService.alterar(produto1);
+
+        Assert.assertEquals("Celular bom", produto1.getNome());
+    }
+
+    @Test
+    public void validaBuscarTodosClientes() {
+        produtos = produtoService.buscarTodos();
+        Assert.assertNotNull(produtos);
     }
 
     @Test
     public void validaExcluirProduto() {
-        produtoService.excluir(produto.getCodigo());
-    }
-
-    @Test
-    public void validaAtualizarProduto() throws TipoChaveNaoEncontradaException {
-        produto.setNome("Celular");
-        produtoService.alterar(produto);
-
-        Assert.assertEquals("Celular", produto.getNome());
+        produtos = produtoService.buscarTodos();
+        produtos.forEach(p -> {
+            produtoService.excluir(p.getCodigo());
+            Produto produtoConsultado = produtoService.consultar(p.getCodigo());
+        });
     }
 
 }
