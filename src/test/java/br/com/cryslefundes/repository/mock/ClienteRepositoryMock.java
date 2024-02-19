a18995b1027d2ae90031b70e44a4ed87763d95e7
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-19 20:07:33 -0300

    Alterando buscarTodos e consultar

--- src/test/java/br/com/cryslefundes/repository/mock/ClienteRepositoryMock.java
+++ src/test/java/br/com/cryslefundes/repository/mock/ClienteRepositoryMock.java
@@ -1,15 +1,18 @@
 package test.java.br.com.cryslefundes.repository.mock;
 
 import main.java.br.com.cryslefundes.domain.Cliente;
-import main.java.br.com.cryslefundes.exceptions.TipoChaveNaoEncontradaException;
 import main.java.br.com.cryslefundes.repository.IClienteRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ClienteRepositoryMock implements IClienteRepository {
 
+    Cliente cliente1 = new Cliente();
+    Cliente cliente2 = new Cliente();
+
     @Override
-    public Boolean cadastrar(Cliente entidade) throws TipoChaveNaoEncontradaException {
+    public Boolean cadastrar(Cliente entidade) {
         return true;
     }
 
@@ -19,19 +22,32 @@ public class ClienteRepositoryMock implements IClienteRepository {
     }
 
     @Override
-    public void alterar(Cliente entidade) throws TipoChaveNaoEncontradaException {
+    public void alterar(Cliente entidade) {
 
     }
 
     @Override
     public Cliente consultar(Long valor) {
-        Cliente cliente = new Cliente();
-        cliente.setCpf(valor);
-        return cliente;
+
+        if (cliente1.getCpf().equals(valor)) {
+            return cliente1;
+        } else if (cliente2.getCpf().equals(valor)) {
+            return cliente2;
+        }
+        return null;
     }
 
     @Override
     public Collection<Cliente> buscarTodos() {
+        Collection<Cliente> clientes = new ArrayList<>();
+
+        if (cliente1 != null && cliente2 != null) {
+            cliente1.setCpf(34123L);
+            cliente2.setCpf(43151L);
+            clientes.add(cliente1);
+            clientes.add(cliente2);
+            return clientes;
+        }
         return null;
     }
 }
--- src/test/java/br/com/cryslefundes/repository/mock/ProdutoRepositoryMock.java
+++ src/test/java/br/com/cryslefundes/repository/mock/ProdutoRepositoryMock.java
@@ -1,14 +1,17 @@
 package test.java.br.com.cryslefundes.repository.mock;
 
 import main.java.br.com.cryslefundes.domain.Produto;
-import main.java.br.com.cryslefundes.exceptions.TipoChaveNaoEncontradaException;
 import main.java.br.com.cryslefundes.repository.IProdutoRepository;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ProdutoRepositoryMock implements IProdutoRepository {
+    Produto produto1 = new Produto();
+    Produto produto2 = new Produto();
+
     @Override
-    public Boolean cadastrar(Produto entidade) throws TipoChaveNaoEncontradaException {
+    public Boolean cadastrar(Produto entidade) {
         return true;
     }
 
@@ -18,19 +21,31 @@ public class ProdutoRepositoryMock implements IProdutoRepository {
     }
 
     @Override
-    public void alterar(Produto entidade) throws TipoChaveNaoEncontradaException {
+    public void alterar(Produto entidade) {
 
     }
 
     @Override
     public Produto consultar(Long valor) {
-        Produto produto1 = new Produto();
-        produto1.setCodigo(valor);
-        return produto1;
+        if (produto1.getCodigo().equals(valor)) {
+            return produto1;
+        } else if (produto2.getCodigo().equals(valor)) {
+            return produto2;
+        }
+        return null;
     }
 
     @Override
     public Collection<Produto> buscarTodos() {
+        Collection<Produto> produtos = new ArrayList<>();
+
+        if (produto1 != null && produto2 != null) {
+            produto1.setCodigo(34123L);
+            produto2.setCodigo(43151L);
+            produtos.add(produto1);
+            produtos.add(produto2);
+            return produtos;
+        }
         return null;
     }
 }
