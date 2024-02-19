7b8f0d46a6f70a6082793df3857b5df4795e40cc
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-19 20:03:57 -0300

    Adicionando metodos obrigatorios de GenericRepository

--- src/main/java/br/com/cryslefundes/repository/ClienteRepository.java
+++ src/main/java/br/com/cryslefundes/repository/ClienteRepository.java
@@ -3,6 +3,9 @@ package main.java.br.com.cryslefundes.repository;
 import main.java.br.com.cryslefundes.domain.Cliente;
 import main.java.br.com.cryslefundes.repository.generic.GenericRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ClienteRepository extends GenericRepository<Cliente, Long> implements IClienteRepository{
 
     public ClienteRepository() {
@@ -15,11 +18,64 @@ public class ClienteRepository extends GenericRepository<Cliente, Long> implemen
     }
 
     @Override
-    public void atualizarDados(Cliente entidade, Cliente entidadeCadastrado) {
-        entidadeCadastrado.setCpf(entidade.getCpf());
-        entidadeCadastrado.setNome(entidade.getNome());
-        entidadeCadastrado.setCidade(entidade.getCidade());
-        entidadeCadastrado.setEndereco(entidade.getEndereco());
-        entidadeCadastrado.setTelefone(entidade.getTelefone());
+    protected String getInsertQuery() {
+        return """
+                INSERT INTO CLIENTES
+                (ID, CPF, NOME, TELEFONE, EMAIL, ENDERECO, CIDADE, ESTADO)
+                VALUES (nextval('sq_clientes'),?,?,?,?,?,?,?);
+                """;
     }
+
+    @Override
+    protected String getUpdateQuery() {
+        return """
+                UPDATE CLIENTES SET
+                NOME = ?, TELEFONE = ?, EMAIL = ?, ENDERECO = ?, CIDADE = ?, ESTADO = ?
+                WHERE CPF = ?;
+                """;
+    }
+
+    @Override
+    protected String getDeleteQuery() {
+        return "DELETE FROM CLIENTES WHERE CPF = ?;";
+    }
+
+    @Override
+    protected String getSelectQuery() {
+        return "SELECT * FROM CLIENTES WHERE CPF = ?;";
+    }
+
+    @Override
+    protected void setInsertQueryParameters(PreparedStatement statement, Cliente entidade) throws SQLException {
+        statement.setLong(1, entidade.getCpf());
+        statement.setString(2, entidade.getNome());
+        statement.setString(3, entidade.getTelefone());
+        statement.setString(4, entidade.getEmail());
+        statement.setString(5, entidade.getEndereco());
+        statement.setString(6, entidade.getCidade());
+        statement.setString(7, entidade.getEstado());
+    }
+
+    @Override
+    protected void setUpdateQueryParameters(PreparedStatement statement, Cliente entidade) throws SQLException{
+        statement.setString(1, entidade.getNome());
+        statement.setString(2, entidade.getTelefone());
+        statement.setString(3, entidade.getEmail());
+        statement.setString(4, entidade.getEndereco());
+        statement.setString(5, entidade.getCidade());
+        statement.setString(6, entidade.getEstado());
+        statement.setLong(7, entidade.getCpf());
+    }
+
+    @Override
+    protected void setDeleteQueryParameters(PreparedStatement statement, Long valor) throws SQLException {
+        statement.setLong(1, valor);
+    }
+
+    @Override
+    protected void setSelectQueryParameters(PreparedStatement statement, Long valor) throws SQLException {
+        statement.setLong(1, valor);
+    }
+
+
 }
--- src/main/java/br/com/cryslefundes/repository/ProdutoRepository.java
+++ src/main/java/br/com/cryslefundes/repository/ProdutoRepository.java
@@ -3,6 +3,9 @@ package main.java.br.com.cryslefundes.repository;
 import main.java.br.com.cryslefundes.domain.Produto;
 import main.java.br.com.cryslefundes.repository.generic.GenericRepository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ProdutoRepository extends GenericRepository<Produto, Long> implements IProdutoRepository {
 
     public ProdutoRepository() {
@@ -15,10 +18,58 @@ public class ProdutoRepository extends GenericRepository<Produto, Long> implemen
     }
 
     @Override
-    public void atualizarDados(Produto entidade, Produto entidadeCadastrado) {
-        entidadeCadastrado.setNome(entidade.getNome());
-        entidadeCadastrado.setCodigo(entidade.getCodigo());
-        entidadeCadastrado.setDescricao(entidade.getDescricao());
-        entidadeCadastrado.setValor(entidade.getValor());
+    protected String getInsertQuery() {
+        return """
+                INSERT INTO PRODUTOS
+                (ID, CODIGO, NOME, DESCRICAO, VALOR, ESTOQUE)
+                VALUES (nextval('sq_produtos'),?,?,?,?,?);
+                """;
+    }
+
+    @Override
+    protected String getUpdateQuery() {
+        return """
+                UPDATE PRODUTOS SET
+                NOME = ?, DESCRICAO = ?, VALOR = ?, ESTOQUE = ?
+                WHERE CODIGO = ?;
+                """;
+    }
+
+    @Override
+    protected String getDeleteQuery() {
+        return "DELETE FROM PRODUTOS WHERE CODIGO = ?";
+    }
+
+    @Override
+    protected String getSelectQuery() {
+        return "SELECT * FROM PRODUTOS WHERE CODIGO = ?";
+    }
+
+    @Override
+    protected void setInsertQueryParameters(PreparedStatement statement, Produto entidade) throws SQLException {
+        statement.setLong(1, entidade.getCodigo());
+        statement.setString(2, entidade.getNome());
+        statement.setString(3, entidade.getDescricao());
+        statement.setBigDecimal(4, entidade.getValor());
+        statement.setInt(5, entidade.getEstoque());
+    }
+
+    @Override
+    protected void setUpdateQueryParameters(PreparedStatement statement, Produto entidade) throws SQLException {
+        statement.setString(1, entidade.getNome());
+        statement.setString(2, entidade.getDescricao());
+        statement.setBigDecimal(3, entidade.getValor());
+        statement.setInt(4, entidade.getEstoque());
+        statement.setLong(5, entidade.getCodigo());
+    }
+
+    @Override
+    protected void setDeleteQueryParameters(PreparedStatement statement, Long valor) throws SQLException {
+        statement.setLong(1, valor);
+    }
+
+    @Override
+    protected void setSelectQueryParameters(PreparedStatement statement, Long valor) throws SQLException {
+        statement.setLong(1, valor);
     }
 }
