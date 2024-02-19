f9d1b4e54b8cf1b3b1df4e0ab062b33daab9882b
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-19 19:54:59 -0300

    Criando a excecao relacionada a tabela

---
+++ src/main/java/br/com/cryslefundes/exceptions/TableException.java
@@ -0,0 +1,11 @@
+package main.java.br.com.cryslefundes.exceptions;
+
+import java.io.Serial;
+
+public class TableException extends Exception {
+    @Serial
+    private static final long serialVersionUID = -7509649433607067138L;
+    public TableException(String msg) {
+        super(msg);
+    }
+}
