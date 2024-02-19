007b7ba6a909c6375031d39f6f1c1ce8157c0740
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-19 19:54:10 -0300

    Adicionando atributo estoque e anotacoes

--- src/main/java/br/com/cryslefundes/domain/Produto.java
+++ src/main/java/br/com/cryslefundes/domain/Produto.java
@@ -1,16 +1,35 @@
 package main.java.br.com.cryslefundes.domain;
 
-import main.java.annotation.TipoChave;
+import main.java.annotation.ColunaTabela;
+import main.java.annotation.Tabela;
 import main.java.br.com.cryslefundes.repository.Persistente;
 
 import java.math.BigDecimal;
 
+@Tabela("PRODUTOS")
 public class Produto implements Persistente {
-    @TipoChave("getCodigo")
+    @ColunaTabela(dbName = "id", setJavaName = "setId")
+    private Long id;
+    @ColunaTabela(dbName = "codigo", setJavaName = "setCodigo")
     private Long codigo;
+    @ColunaTabela(dbName = "nome", setJavaName = "setNome")
     private String nome;
+    @ColunaTabela(dbName = "descricao", setJavaName = "setDescricao")
     private String descricao;
+    @ColunaTabela(dbName = "valor", setJavaName = "setValor")
     private BigDecimal valor;
+    @ColunaTabela(dbName = "estoque", setJavaName = "setEstoque")
+    private Integer estoque;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getCodigo() {
         return codigo;
@@ -38,6 +57,14 @@ public class Produto implements Persistente {
 
     public BigDecimal getValor() {
         return valor;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
     }
 
     public void setValor(BigDecimal valor) {
